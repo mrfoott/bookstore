f4690a1dadd9a0eab098bb6cced89e9fa817e02a
nhat <nhatlx159@gmail.com>
2022-10-26 20:29:04 +0700

    build Routes/userRoute.js add connectionDB.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const userRoute = require("./Routes/userRoute")
 const app = express()
 const port = process.env.PORT || 3000
 
+app.get("/", userRoute)
 
 app.listen(port, ()=> {
     console.log("Server listening on: " + port);
